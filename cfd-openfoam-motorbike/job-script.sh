930cdb3e9f665e666beb0dbf5f0024fbc269dd51
jamesbeedy <jamesbeedy@gmail.com>
2025-02-14 17:42:09 +0000

    add apptainer environment variables and touch ups to job script

--- cfd-openfoam-motorbike/job-script.sh
+++ cfd-openfoam-motorbike/job-script.sh
@@ -1,56 +1,63 @@
 #!/bin/bash
 #SBATCH --ntasks=8
 #SBATCH -J motorbike
-#SBATCH --output=/nfs/mnt/motorbike/R-%x.%j.out
-#SBATCH --error=/nfs/mnt/motorbike/R-%x.%j.err
+#SBATCH --output=/nfs/mnt/jobs/%u/%x-Job-%j/R-%x.%j.out
+#SBATCH --error=/nfs/mnt/jobs/%u/%x-Job-%j/R-%x.%j.err
 #SBATCH -t 8:00:00
+#SBATCH --get-user-env
+
+export APPTAINER_DIR=/nfs/mnt/apptainer
+
+export APPTAINER_CACHEDIR=$APPTAINER_DIR/cache
+export APPTAINER_TMPDIR=$APPTAINER_DIR/tmp
+export APPTAINER_IMAGES_DIR=$APPTAINER_DIR/images
+export APPTAINER_BINDPATH="/nfs"
+export TMPDIR=$APPTAINER_TMPDIR
+
+export REMOTE_APPTAINER_IMAGE="oras://public.ecr.aws/omnivector-solutions/cfd-openfoam-motorbike:latest"
+export LOCAL_APPTAINER_IMAGE=$APPTAINER_IMAGES_DIR/vantage-jobs-catalog-openfoam11.sif
+export WORK_DIR=/nfs/mnt/jobs/$USER/$SLURM_JOB_NAME-Job-$SLURM_JOB_ID
+
+mkdir -p $APPTAINER_TMPDIR $APPTAINER_CACHEDIR $APPTAINER_IMAGES_DIR $WORK_DIR
 
 # clone OpenFOAM-11 if it is not available yet
-OPENFOAM_DIR=/nfs/OpenFOAM-11
+export OPENFOAM_DIR=/nfs/mnt/jobs/$USER/OpenFOAM-11
 if [[ ! -d $OPENFOAM_DIR ]]
 then
     echo "Cloning OpenFOAM-11"
-    cd /nfs/
-    git clone https://github.com/OpenFOAM/OpenFOAM-11
+    git clone https://github.com/OpenFOAM/OpenFOAM-11 $OPENFOAM_DIR
 else
     echo "Skipping clone process...we already have the OpenFOAM-11 source code"
 fi
 
-# create a working folder inside the shared directory
-WORK_DIR=/nfs/mnt/motorbike/$SLURM_JOB_NAME-Job-$SLURM_JOB_ID
-mkdir -p $WORK_DIR
-cd $WORK_DIR
-
-# path to the openfoam singularity image
-export APPTAINER_IMAGE=/nfs/mnt/openfoam11.sif
-
-# download the openfoam v11 singularity image if it is not available yet
-if [[ ! -f $APPTAINER_IMAGE ]]
+# download the singularity image if it is not available yet
+if [[ ! -f $LOCAL_APPTAINER_IMAGE ]]
 then
-    echo "Pulling the singularity image for OpenFOAM-11"
-    apptainer pull $APPTAINER_IMAGE oras://public.ecr.aws/omnivector-solutions/cfd-openfoam-motorbike:latest
+    echo "Pulling the singularity image"
+    apptainer pull $LOCAL_APPTAINER_IMAGE $REMOTE_APPTAINER_IMAGE
 else
     echo "Skipping the image fetch process...we already have the singularity image"
 fi
 
+cd $WORK_DIR
 
 # copy motorBike folder
 cp -r $OPENFOAM_DIR/tutorials/incompressibleFluid/motorBike/motorBike .
 
 cd motorBike
 
-apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE ./Allclean
+apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE ./Allclean
 
 cp $OPENFOAM_DIR/tutorials/resources/geometry/motorBike.obj.gz constant/geometry/
 
-apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE blockMesh
+apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE blockMesh
 
-apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE decomposePar -copyZero
+apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE decomposePar -copyZero
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE snappyHexMesh -overwrite -parallel
+mpirun --np 8 apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE snappyHexMesh -overwrite -parallel
 
 find . -type f -iname "*level*" -exec rm {} \;
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE renumberMesh -overwrite
+mpirun --np 8 apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE renumberMesh -overwrite
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE potentialFoam -initialiseUBCs -parallel
+mpirun --np 8 apptainer exec --bind $PWD:$HOME $LOCAL_APPTAINER_IMAGE potentialFoam -initialiseUBCs -parallel
--- hpl-benchmark-aocc/hpl-aocc.sh
+++ hpl-benchmark-aocc/hpl-aocc.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-#SBATCH -J HPL_oacc
+#SBATCH -J HPL_aocc
 #SBATCH --ntasks=2
-#SBATCH --output=/nfs/mnt/hpl_aocc/%x-Job-%j/R-%x.%j.out
-#SBATCH --error=/nfs/mnt/hpl_aocc/%x-Job-%j/R-%x.%j.err
+#SBATCH --output=/nfs/mnt/jobs/%u/%x-Job-%j/R-%x.%j.out
+#SBATCH --error=/nfs/mnt/jobs/%u/%x-Job-%j/R-%x.%j.err
 #SBATCH -t 00:05:00
+#SBATCH --get-user-env
 
-WORK_DIR=/nfs/mnt/hpl_aocc/$SLURM_JOB_NAME-Job-$SLURM_JOB_ID
-mkdir -p $WORK_DIR
+
+export APPTAINER_DIR=/nfs/mnt/apptainer
+
+export APPTAINER_CACHEDIR=$APPTAINER_DIR/cache
+export APPTAINER_TMPDIR=$APPTAINER_DIR/tmp
+export APPTAINER_IMAGES_DIR=$APPTAINER_DIR/images
+export APPTAINER_BINDPATH=/nfs
+export TMPDIR=$APPTAINER_TMPDIR
+
+export REMOTE_APPTAINER_IMAGE="oras://public.ecr.aws/omnivector-solutions/hpl-benchmark-aocc:latest"
+export LOCAL_APPTAINER_IMAGE=$APPTAINER_IMAGES_DIR/vantage-jobs-catalog-hpl-aocc.sif
+export WORK_DIR=/nfs/mnt/jobs/$USER/$SLURM_JOB_NAME-Job-$SLURM_JOB_ID
+
+mkdir -p $APPTAINER_TMPDIR $APPTAINER_CACHEDIR $APPTAINER_IMAGES_DIR $WORK_DIR
+
+# move the HPCG.dat top the Workdir
 cd $WORK_DIR
-
-# move the HPL.dat to the Workdir
 mv $SLURM_SUBMIT_DIR/HPL.dat.txt ./HPL.dat
 
-export APPTAINER_IMAGE=/nfs/mnt/hpl_aocc.sif
-
 # download the singularity image if it is not available yet
-if [[ ! -f $APPTAINER_IMAGE ]]
+if [[ ! -f $LOCAL_APPTAINER_IMAGE ]]
 then
     echo "Pulling the singularity image"
-    apptainer pull $APPTAINER_IMAGE oras://public.ecr.aws/omnivector-solutions/hpl-benchmark-aocc:latest
+    apptainer pull $LOCAL_APPTAINER_IMAGE $REMOTE_APPTAINER_IMAGE
 else
     echo "Skipping the image fetch process...we already have the singularity image"
 fi
 
-apptainer exec $APPTAINER_IMAGE bash -c "source /entrypoint.sh && xhpl"
-
-
+apptainer exec $LOCAL_APPTAINER_IMAGE sh -c "/hpl/bin/ubuntu/xhpl"
