2005457019fa8f465d69ddc4b901911e39352fcd
Matheus Tosta <me@mtosta.dev>
2025-03-14 10:33:32 -0400

    fix(cfd-openfoam-motorbike): adjust the simulation parameters accordingly the job config.
    This commit adjusts the motorbike simulation script so the simulation parameters are set
    accordingly the number of nodes and tasks set for the job.

--- cfd-openfoam-motorbike/job-script.sh
+++ cfd-openfoam-motorbike/job-script.sh
@@ -4,14 +4,14 @@
 #SBATCH --output=/nfs/mnt/motorbike/R-%x.%j.out
 #SBATCH --error=/nfs/mnt/motorbike/R-%x.%j.err
 #SBATCH -t 8:00:00
+#SBATCH --get-user-env
 
 # clone OpenFOAM-11 if it is not available yet
-OPENFOAM_DIR=/nfs/OpenFOAM-11
+OPENFOAM_DIR=/nfs/mnt/OpenFOAM-11
 if [[ ! -d $OPENFOAM_DIR ]]
 then
     echo "Cloning OpenFOAM-11"
-    cd /nfs/
-    git clone https://github.com/OpenFOAM/OpenFOAM-11
+    git clone https://github.com/OpenFOAM/OpenFOAM-11 $OPENFOAM_DIR
 else
     echo "Skipping clone process...we already have the OpenFOAM-11 source code"
 fi
@@ -39,6 +39,15 @@ cp -r $OPENFOAM_DIR/tutorials/incompressibleFluid/motorBike/motorBike .
 
 cd motorBike
 
+# adjust the decompose dict according to the number of processes in the job
+nnodes=$SLURM_NNODES
+ntasks=$SLURM_NTASKS
+ntasks_per_node=$((ntasks / nnodes))
+new_subdomains=$ntasks
+new_n="($nnodes $ntasks_per_node 1)"
+sed -i "s/numberOfSubdomains \+[0-9]\+;/numberOfSubdomains $new_subdomains;/g" system/decomposeParDict
+sed -i "s/n \+ (.*);/n $new_n;/g" system/decomposeParDict
+
 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE ./Allclean
 
 cp $OPENFOAM_DIR/tutorials/resources/geometry/motorBike.obj.gz constant/geometry/
@@ -47,10 +56,10 @@ apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE blockMesh
 
 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE decomposePar -copyZero
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE snappyHexMesh -overwrite -parallel
+mpirun --np $ntasks apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE snappyHexMesh -overwrite -parallel
 
 find . -type f -iname "*level*" -exec rm {} \;
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE renumberMesh -overwrite
+mpirun --np $ntasks apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE renumberMesh -overwrite -parallel
 
-mpirun --np 8 apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE potentialFoam -initialiseUBCs -parallel
+mpirun --np $ntasks apptainer exec --bind $PWD:$HOME $APPTAINER_IMAGE potentialFoam -initialiseUBCs -parallel
